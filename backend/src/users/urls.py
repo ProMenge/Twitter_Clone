7c22aa204bf930b38d08f816ba89f4911a0bbeae
Fred Menge <mengeprofissional@gmail.com>
2025-07-16 11:31:57 -0300

    feat: change password feature

--- backend/src/users/urls.py
+++ backend/src/users/urls.py
@@ -8,17 +8,20 @@ from .views import (
     follow_user,
     unfollow_user,
     who_to_follow,
+    PasswordChangeView,
 )
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('me/', MeView.as_view(), name='me'),
-    path('users/<int:id>/', UserUpdateView.as_view(), name='user-update'), # PUT para atualizar o próprio usuário
-    path('users/<int:id>/follow/', follow_user, name='follow-user'), # POST para seguir
-    # DELETE para deixar de seguir
+    
+    path('change-password/', PasswordChangeView.as_view(), name='change-password'),
+    path('users/<int:id>/', UserUpdateView.as_view(), name='user-update'),
+    path('users/<int:id>/follow/', follow_user, name='follow-user'),
     path('users/<int:id>/unfollow/', unfollow_user, name='unfollow-user'),
-    path('users/<str:username>/', UserDetailView.as_view(), name='user-detail'), # GET para detalhes de perfil por username
+    
+    path('users/<str:username>/', UserDetailView.as_view(), name='user-detail'),
     
     path('who-to-follow/', who_to_follow, name='who-to-follow'),
 ]
